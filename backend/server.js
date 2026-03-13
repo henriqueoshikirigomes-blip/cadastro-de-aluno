61b27455590740f162b073a29c200c6f0899a1e3
henriqueoshikirigomes-blip <henrique.oshikiri.gomes@escola.pr.gov.br>
2026-03-13 10:59:48 -0300

    Implement POST endpoint for saving student data
    Added a POST endpoint to save student data to the database.

--- backend/server.js
+++ backend/server.js
@@ -12,3 +12,22 @@ const conexao = mySql.createConnection({
     password: '',
     database: 'escola'
 });
+
+app.post("/salvar", (req, res) => {
+    let nome = req.body.nome;
+    let idade = red.body.idade;
+
+    let sql = "INSERT INTO alunos (nome, idade)VALUES (?, ?);"
+
+    conexao.query(sql, [nome, idade], (erro, resultado) => {
+        if(erro) {
+            console.log(erro);
+        } else {
+            res.send("Aluno salvo com sucesso");
+        }
+    });
+});
+
+app.listen(3000, () =>{
+    console.log("servidor rodando naporta 3000");
+});
